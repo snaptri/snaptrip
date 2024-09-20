56069f7a5085410acfaa9cde31c3bb5366b43f2b
Jean <jeanpolo15@gmail.com>
2024-09-19 21:26:08 -0500

    Add: adición de nuevas validaciones

--- frontend/src/pages/Home/ContactForm.jsx
+++ frontend/src/pages/Home/ContactForm.jsx
@@ -8,7 +8,7 @@ import {
 	TextField,
 	Typography,
 } from '@mui/material'
-import { useForm, Controller } from 'react-hook-form'
+import { useForm } from 'react-hook-form'
 
 export const ContactForm = () => {
 	const {
@@ -21,7 +21,8 @@ export const ContactForm = () => {
 	const onSubmit = handleSubmit((data) => {
 		// eslint-disable-next-line no-console
 		console.log('formulario enviado', data)
-		reset()
+		// eslint-disable-next-line no-alert
+		alert('Su formulario ha sido enviado conrrectamente')
 	})
 
 	const StyledTextField = styled(TextField)`
@@ -67,39 +68,80 @@ export const ContactForm = () => {
 				Contactanos:
 			</Typography>
 			<StyledTextField
+				type="text"
 				label="Nombre"
 				variant="outlined"
 				{...register('nombre', {
-					required: true,
+					required: {
+						value: true,
+						message: 'Este campo es requerido',
+					},
+					minLength: {
+						value: 2,
+						message: 'Debe tener mas de 2 caracteres',
+					},
+					maxLength: {
+						value: 20,
+						message: 'Máximo de 20 caracteres',
+					},
+					pattern: {
+						value: /^[A-Za-zÀ-ÿ\s'-]+$/,
+						message:
+							'No se permiten caracteres especiales ni números',
+					},
 				})}
 			/>
 			{errors.nombre && (
-				<span style={{ color: 'red', fontSize: '11px' }}>
-					Este campo es requerido
+				<span style={{ color: '#f23d30', fontSize: '11px' }}>
+					{errors.nombre.message}
 				</span>
 			)}
 			<StyledTextField
+				type="text"
 				label="Email"
 				variant="outlined"
 				{...register('email', {
-					required: true,
+					required: {
+						value: true,
+						message: 'Este campo es requerido',
+					},
+					maxLength: {
+						value: 150,
+						message: 'Máximo de 150 caracteres',
+					},
+					pattern: {
+						value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+						message: 'Formato de correo electrónico inválido',
+					},
 				})}
 			/>
 			{errors.email && (
-				<span style={{ color: 'red', fontSize: '11px' }}>
-					Este campo es requerido
+				<span style={{ color: '#f23d30', fontSize: '11px' }}>
+					{errors.email.message}
 				</span>
 			)}
 			<StyledTextField
+				type="text"
 				label="Mensaje"
 				variant="outlined"
 				{...register('mensaje', {
-					required: true,
+					required: {
+						value: true,
+						message: 'Este campo es requerido',
+					},
+					maxLength: {
+						value: 150,
+						message: 'Máximo de 150 caracteres',
+					},
+					minLength: {
+						value: 10,
+						message: 'Debe tener mas de 10 caracteres',
+					},
 				})}
 			/>
 			{errors.mensaje && (
-				<span style={{ color: 'red', fontSize: '11px' }}>
-					Este campo es requerido
+				<span style={{ color: '#f23d30', fontSize: '11px' }}>
+					{errors.mensaje.message}
 				</span>
 			)}
 			<button
